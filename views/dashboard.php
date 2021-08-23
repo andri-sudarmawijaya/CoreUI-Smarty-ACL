4d60c1fe3c79e9743fab31c4a01962adcdf68136
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2021-08-23 21:57:40 +0700

    update dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -12,52 +12,52 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <html lang="en">
 
 <head>
-    <?php $this->load->view('theme-core-ui/_partials/_head'); ?>
+    <?php $this->load->view('_partials/_head'); ?>
 </head>
 
 <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
     <header class="app-header navbar _header">
-        <?php $this->load->view('theme-core-ui/_partials/_header'); ?>
+        <?php $this->load->view('_partials/_header'); ?>
     </header>
     <div class="app-body" id="pjax-container">
         <div class="sidebar _sidebar">
-            <?php $this->load->view('theme-core-ui/_partials/_sidebar'); ?>
+            <?php $this->load->view('_partials/_sidebar'); ?>
         </div>
         <main class="main">
             <!-- Breadcrumb-->
             <ol class="breadcrumb">
-                <?php $this->load->view('theme-core-ui/_partials/_breadcrumb'); ?>
+                <?php $this->load->view('_partials/_breadcrumb'); ?>
             </ol>
             <div class="container-fluid">
                 <div class="animated fadeIn">
                     <div class="row _highlight">
-                        <?php $this->load->view('theme-core-ui/_partials/_highlight'); ?>
+                        <?php $this->load->view('_partials/_highlight'); ?>
                     </div>
                     <!-- /.row-->
                     <div class="card _content">
-                        <?php $this->load->view('theme-core-ui/_partials/_content'); ?>
+                        <?php $this->load->view('_partials/_content'); ?>
                     </div>
                     <!-- /.card-->
                     <div class="row _top">
-                        <?php $this->load->view('theme-core-ui/_partials/_top'); ?>
+                        <?php $this->load->view('_partials/_top'); ?>
                     </div>
                     <!-- /.row-->
                     <div class="row _bottom">
-                        <?php $this->load->view('theme-core-ui/_partials/_bottom'); ?>
+                        <?php $this->load->view('_partials/_bottom'); ?>
                     </div>
                     <!-- /.row-->
                 </div>
             </div>
         </main>
         <aside class="aside-menu _aside">
-            <?php $this->load->view('theme-core-ui/_partials/_aside'); ?>
+            <?php $this->load->view('_partials/_aside'); ?>
         </aside>
     </div>
     <footer class="app-footer _footer">
-        <?php $this->load->view('theme-core-ui/_partials/_footer'); ?>
+        <?php $this->load->view('_partials/_footer'); ?>
     </footer>
     <!-- CoreUI and necessary plugins-->
-    <?php $this->load->view('theme-core-ui/_partials/_script'); ?>
+    <?php $this->load->view('_partials/_script'); ?>
 
     <!-- Page necessary plugins-->
 
